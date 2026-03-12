ae9b4983c909fc6fe9bda124cfb759d7b26595d9
Mac <mac@Hubo.local>
2026-03-12 08:56:30 +0800

    Add build for GitHub Pages

---
+++ dist/assets/vendor-react--tiHe_59.js
@@ -0,0 +1,1 @@
+import"./vendor-router-DuwlbEED.js";
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+  base: '/NIUBEER/',
   plugins: [react()],
   resolve: {
     alias: {
