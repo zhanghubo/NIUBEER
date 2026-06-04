a46e06a6b6589ccc0d3dbc3ec08a0726da2305a3
Mac <mac@Hubo-7.local>
2026-06-04 16:00:08 +0800

    Fix Vercel root routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="./">
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
